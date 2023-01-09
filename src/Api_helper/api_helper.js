d4bc95fb19516a59096d4384310d85e7471430d6
Sagar Kale <95963864+skale0181@users.noreply.github.com>
2023-01-10 00:20:49 +0530

    Update api_helper.js
    update server api in code

--- src/Api_helper/api_helper.js
+++ src/Api_helper/api_helper.js
@@ -1,20 +1,21 @@
 // root api 
 // const ROOT = "https://urban-touch-0181.herokuapp.com"
-const ROOT = "http://localhost:4000"
+// const ROOT = "http://localhost:4000"
+const ROOT = "https://urban-touch-skale0181.onrender.com/"
 
-// authentication https://urban-touch-0181.herokuapp.com/cart/ 
-export const LOGIN = ROOT+"/login";
-export const REGISTER = ROOT+"/register";
+// authentication 
+export const LOGIN = ROOT+"login";
+export const REGISTER = ROOT+"register";
 export const SIGN_OUT = ROOT+"";
 
 // products api for home page 
-export const MENS_APPARELS_FRONT = ROOT+"/mennapparel";
-export const WOMEN_APPARELS_FRONT = ROOT+"/womennapparel";
-export const KIDS_APPARELS_FRONT = ROOT+"/kidapparel";
-export const GIFT_APPARELS_FRONT = ROOT+"/giftapparel";
+export const MENS_APPARELS_FRONT = ROOT+"mennapparel";
+export const WOMEN_APPARELS_FRONT = ROOT+"womennapparel";
+export const KIDS_APPARELS_FRONT = ROOT+"kidapparel";
+export const GIFT_APPARELS_FRONT = ROOT+"giftapparel";
 // products api
-export const MENS_APPARELS = ROOT+"/mens";
-export const WOMEN_APPARELS = ROOT+"/womens";
-export const KIDS_APPARELS = ROOT+"/kids";
-export const GIFT_APPARELS = ROOT+"/gifts";
-export const CART = ROOT+"/cart/";
+export const MENS_APPARELS = ROOT+"mens";
+export const WOMEN_APPARELS = ROOT+"womens";
+export const KIDS_APPARELS = ROOT+"kids";
+export const GIFT_APPARELS = ROOT+"gifts";
+export const CART = ROOT+"cart/";
